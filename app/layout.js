aa0582528beb2540ffc59b9f6c815e5097339478
Davide Colombi <cillo@Davides-MBP.station>
2026-09-14 15:29:31 +0200

    redesign portfolio2026

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import CustomCursor from "@/components/customCursor";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,13 +18,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <CustomCursor />
-        {children}
-      </body>
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
